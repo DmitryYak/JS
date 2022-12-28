8af1f392399fdbecf92b8aab06e43e8b61e550bd
Dmitry Yakovlev <100353974+DmitryYak@users.noreply.github.com>
2022-12-28 19:17:13 +0300

    Add files via upload

---
+++ vladilen_1_let_const.js
@@ -0,0 +1,43 @@
+var num = 42;
+
+num = "str";
+
+console.log(num);
+
+// видимость а только в скобках:
+// if (true) {
+//   let a = 42;
+// }
+
+// console.log(a);
+
+//   Hosting:
+
+// console.log(b);
+// var b = 10;
+
+// function hoisted() {
+//   age = 26;
+// }
+
+// let age;
+// hoisted();
+// console.log(age);
+
+//   Const
+// const COLOR = "#ffeebb";
+// console.log(COLOR);
+// COLOR = "#000";   //error
+
+// const array = [1, 2, 3, 5, 8];
+
+// console.log(array);
+
+// array.push(13);
+
+// console.log(array);
+
+const obj = { a: 42 };
+obj.name = "Igor";
+
+console.log(obj);

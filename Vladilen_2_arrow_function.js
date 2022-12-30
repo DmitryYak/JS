1b3134e6159ab4689146915d8c7e33efa2bdc751
Dmitry Yakovlev <100353974+DmitryYak@users.noreply.github.com>
2022-12-30 09:04:35 +0300

    Add files via upload

---
+++ Vladilen_2_arrow_function.js
@@ -0,0 +1,47 @@
+// function sum(a, b) {
+//   return a + b;
+// }
+
+// function cube(a) {
+//   return a ** 3;
+// }
+
+// const sum = (a, b) => {
+// return a + b
+// }
+
+// const cube = (a) => {
+//     return a ** 3
+// }
+// console.log(sum(a:41, b:1))
+// console.log(cube(a:2))
+
+//var 1
+// setTimeout(function () {
+//   console.log("after 1 second");
+// }, 1000);
+
+//var 2
+// setTimeout(() => console.log("after 1 second"), 1000);
+
+//   Context
+function log() {
+  console.log(this);
+}
+
+const arrowLog = () => console.log(this);
+
+const person = {
+  name: "Elena",
+  age: 20,
+  log: log,
+  arrowLog: arrowLog,
+  delayLog: function () {
+    setTimeout(function () {
+      console.log(this.name + " " + this.age);
+    }, 500);
+  },
+};
+
+// person.log();
+person.delayLog();

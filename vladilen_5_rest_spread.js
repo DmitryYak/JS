57cc34cfb9b244193b32f2e60f414183bbfab5c8
Dmitry Yakovlev <100353974+DmitryYak@users.noreply.github.com>
2023-01-06 12:41:47 +0300

    Update vladilen_5_rest_spread.js

--- vladilen_5_rest_spread.js
+++ vladilen_5_rest_spread.js
@@ -1,7 +1,61 @@
 //  Rest
 
-function average(arr) {
-  return arr.reduce((acc, i) => (acc += i), 0) / arr.length;
-}
+// function average(arr) {
+//   return (
+//     Array.from(arguments).reduce((acc, i) => (acc += i), 0) / arguments.length
+//   );
+// }
 
-console.log(average([10, 20, 30, 40]));
+// console.log(average(10, 20, 30, 40, 50));
+
+// function average(...args) {
+//   return args.reduce((acc, i) => (acc += i), 0) / args.length;
+// }
+
+// console.log(average(10, 20, 30, 40, 50));
+
+// Spread
+// const array = [1, 2, 3, 5, 8, 13];
+// console.log(array);
+
+// const array = [1, 2, 3, 5, 8, 13];
+// // console.log(...array);
+// console.log(Math.max(...array));
+// console.log(Math.min(...array));
+
+// const fib = [1, ...array];
+// console.log(fib);
+
+//   Destructuring
+
+const array = [1, 2, 3, 5, 8, 13];
+//1:
+// const a = array[0]
+// const b = array[1]
+//2:
+const [a, b, ...c] = array;
+console.log(a, b, c);
+
+//  Object
+const address = {
+  country: "Russia",
+  city: "Moscow",
+  street: "Lenina",
+  concat: function () {
+    return `${this.country}, ${this.city}, ${this.street}`;
+  },
+};
+
+// const { city, contry, street = "Tverskaya", concat: addressConcat } = address;
+
+// console.log(addressConcat.call(address));
+// console.log(street);
+
+const { city, ...rest } = address;
+console.log(city);
+console.log(rest);
+
+const newAdress = { ...address, street: "Tverskaya", code: 123 };
+console.log(newAdress);
+
+// console.log(address.concat());
